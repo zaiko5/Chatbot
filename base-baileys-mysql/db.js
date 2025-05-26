4f9c23788515c477b51d3f86eda14babb8294895
Benyi Cordero <benyi.uriel2006@gmail.com>
2025-05-26 10:59:37 -0600

    Hice el start del bot

--- base-baileys-mysql/db.js
+++ base-baileys-mysql/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-    host: process.env.DB_HOST,     
-    user: process.env.DB_USER,    
-    password: process.env.DB_PASS, 
-    database: process.env.DB_NAME, 
+    host: process.env.MYSQL_DB_HOST,
+        user: process.env.MYSQL_DB_USER,
+        database: process.env.MYSQL_DB_NAME,
+        password: process.env.MYSQL_DB_PASSWORD,
+        port: parseInt(process.env.MYSQL_DB_PORT || '3306'),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
