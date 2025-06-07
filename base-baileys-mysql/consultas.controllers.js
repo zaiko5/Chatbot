c624aca6ad7dca6aacacd7a70089f4607e3a89bf
Benyi Cordero <benyi.uriel2006@gmail.com>
2025-06-06 23:59:58 -0600

    Ahora acepta el prompt de la base de datos

--- base-baileys-mysql/consultas.controllers.js
+++ base-baileys-mysql/consultas.controllers.js
@@ -2,7 +2,7 @@ const db = require('./db');
 
 function getCurrentWeek(){
     const today = new Date();
-    const firstDayOfYear = new Date(today.getFullYear, 0, 1);
+    const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
     const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
     return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1 ) / 7);
 }
